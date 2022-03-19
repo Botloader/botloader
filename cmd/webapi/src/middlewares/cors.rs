fc267407a022fb377e776a290b4c9cd87209e065
jonastar <98033487+jonastar@users.noreply.github.com>
2022-03-19 18:28:44 +0100

    webapi: allows cors on localhost to ease frontend development

--- cmd/webapi/src/middlewares/cors.rs
+++ cmd/webapi/src/middlewares/cors.rs
@@ -50,19 +50,33 @@ where
         let clone = self.inner.clone();
         let mut inner = std::mem::replace(&mut self.inner, clone);
 
-        let host_base = self.run_config.frontend_host_base.clone();
+        let prod_host_base = self.run_config.frontend_host_base.clone();
+        let origin = req
+            .headers()
+            .get(header::ORIGIN)
+            .map(|v| v.to_str().unwrap_or(""))
+            .unwrap_or("")
+            .to_owned();
+
+        let is_valid_origin = is_origin_allowed(&origin, &prod_host_base);
 
         if matches!(req.method(), &Method::OPTIONS) {
             Box::pin(async move {
                 let mut resp = Response::new(boxed(Empty::new()));
-                insert_headers(&host_base, resp.headers_mut());
+
+                if is_valid_origin {
+                    insert_headers(&origin, resp.headers_mut());
+                }
+
                 Ok(resp)
             })
         } else {
             Box::pin(async move {
                 match inner.call(req).await {
                     Ok(mut resp) => {
-                        insert_headers(&host_base, resp.headers_mut());
+                        if is_valid_origin {
+                            insert_headers(&origin, resp.headers_mut());
+                        }
                         Ok(resp)
                     }
                     Err(err) => Err(err),
@@ -70,6 +84,27 @@ where
             })
         }
     }
+}
+
+fn is_origin_allowed(origin: &str, prod_origin: &str) -> bool {
+    if origin == prod_origin {
+        return true;
+    }
+
+    if origin == "http://localhost"
+        || origin.starts_with("http://localhost:")
+        || origin == "https://localhost"
+        || origin.starts_with("https://localhost:")
+    {
+        // accept localhost domains, the user would have to install something locally for this to
+        // be abused
+        //
+        // this helps a ton as we can develop the frontend against the prod API without the need
+        // for running a local version of the stack
+        return true;
+    }
+
+    false
 }
 
 fn insert_headers(host_base: &str, headers: &mut HeaderMap) {
@@ -89,4 +124,32 @@ fn insert_headers(host_base: &str, headers: &mut HeaderMap) {
         header::ACCESS_CONTROL_ALLOW_CREDENTIALS,
         HeaderValue::from_static("true"),
     );
+}
+
+#[cfg(test)]
+mod tests {
+
+    macro_rules! origin_allowed_test_case {
+        ($actual_origin:literal, $allowed_origin:literal, allow) => {
+            assert_eq!(
+                super::is_origin_allowed($actual_origin, $allowed_origin),
+                true
+            );
+        };
+
+        ($actual_origin:literal, $allowed_origin:literal, deny) => {
+            assert_eq!(
+                super::is_origin_allowed($actual_origin, $allowed_origin),
+                false
+            );
+        };
+    }
+
+    #[test]
+    fn test_is_origin_allowed() {
+        origin_allowed_test_case!("https://botloader.io", "https://botloader.io", allow);
+        origin_allowed_test_case!("https://banana.io", "https://botloader.io", deny);
+        origin_allowed_test_case!("https://localhost:3000", "https://botloader.io", allow);
+        origin_allowed_test_case!("http://localhost", "https://botloader.io", allow);
+    }
 }
