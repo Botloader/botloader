66af7d900639c1657b28987b09231064682ae1bb
jonastar <98033487+jonastar@users.noreply.github.com>
2023-02-13 13:08:52 +0100

    render app properly

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const domNode = document.getElementById('root');
+const root = createRoot(domNode!);
+root.render(<React.StrictMode>
+  <App />
+</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
