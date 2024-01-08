2f44e56a35530c865b25e2952a08751f0a8c07a2
jonastar <98033487+jonastar@users.noreply.github.com>
2024-01-09 00:58:51 +0100

    fix doc generation

--- components/runtime/docgen/src/index.js
+++ components/runtime/docgen/src/index.js
@@ -1,13 +1,7 @@
 import { Application, TSConfigReader, TypeDocReader } from "typedoc";
 
 async function main() {
-    const app = new Application();
-
-    // If you want TypeDoc to load tsconfig.json / typedoc.json files
-    app.options.addReader(new TSConfigReader());
-    app.options.addReader(new TypeDocReader());
-
-    app.bootstrap({
+    const app = await Application.bootstrapWithPlugins({
         // typedoc options here
         entryPoints: ["../src/ts/docs_index.ts"],
         tsconfig: "../src/ts/tsconfig.json",
@@ -18,7 +12,22 @@ async function main() {
         excludeInternal: true,
     });
 
-    const project = app.convert();
+    // If you want TypeDoc to load tsconfig.json / typedoc.json files
+    // app.options.addReader(new TSConfigReader());
+    // app.options.addReader(new TypeDocReader());
+
+    // app.bootstrap({
+    //     // typedoc options here
+    //     entryPoints: ["../src/ts/docs_index.ts"],
+    //     tsconfig: "../src/ts/tsconfig.json",
+    //     readme: "src/README.md",
+    //     name: "Botloader",
+    //     excludePrivate: true,
+    //     excludeProtected: true,
+    //     excludeInternal: true,
+    // });
+
+    const project = await app.convert();
 
     if (project) {
         // Project may not have converted correctly
